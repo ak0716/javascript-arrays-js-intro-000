df4ae399802d4b97e26eb7b84be897e069e7ec4b
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-13 21:08:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 
-function addElementToBeginningOfArray([1], a) {
+function addElementToBeginningOfArray([], a) {
   var myArray = [1];
   return myArray.unshift('foo');
 }
