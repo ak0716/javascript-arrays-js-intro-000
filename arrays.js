cb3800c940751893fc7ff93fedff9f13252ae68b
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-13 21:48:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,16 @@ destructivelyAddElementToBeginningOfArray = (myArray) => {
   myArray.unshift('foo');
   return myArray;
 }
+
+
+addElementToEndOfArray = () => {
+  var myArray = [1];
+  myArray.push('foo');
+  return myArray;
+}
+
+var myArray = [1];
+destructivelyAddElementToEndOfArray = (myArray) => {
+  myArray.push('foo');
+  return myArray;
+}
